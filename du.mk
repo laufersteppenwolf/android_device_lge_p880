71767c3acf01f9df2075ba3fcdb5014413163237
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-06-21 01:21:58 +0200

    DU'ify
    Change-Id: Id215abcf29fc15d5ab1f41af71763834401dd844

--- du.mk
+++ du.mk
@@ -1,6 +1,3 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
@@ -8,11 +5,8 @@ TARGET_SCREEN_WIDTH := 720
 # Release name
 PRODUCT_RELEASE_NAME := p880
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# This device has NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p880/p880.mk)
@@ -23,7 +17,7 @@ PRODUCT_RELEASE_NAME := Optimus 4X HD
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p880
-PRODUCT_NAME := cm_p880
+PRODUCT_NAME := du_p880
 PRODUCT_BRAND := LG
 PRODUCT_MODEL := LG-P880
 PRODUCT_MANUFACTURER := LGE
